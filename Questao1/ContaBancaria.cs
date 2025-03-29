4307b61d1c113b7e52b42b56fde15b3c23844802
AugustinhoCelestino <58995280+AugustinhoCelestino@users.noreply.github.com>
2025-03-29 10:27:58 -0300

    initial

---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,32 @@
+﻿namespace Questao1
+{
+    class ContaBancaria
+    {
+        public ContaBancaria(int numero, string titular, double depositoinicial)
+        {
+            this.Saldo = depositoinicial;
+            this.Numero = numero; 
+            this.Titular = titular;
+        }
+        public ContaBancaria(int numero, string titular)
+        {
+            this.Numero = numero;
+            this.Titular = titular;
+            this.Saldo = 0;
+        }
+
+        public int Numero { get; set; } = 0;
+        public string Titular { get; set; } = string.Empty;
+        public double Saldo { get; set; } = 0;
+
+        public void Deposito(double quantia)
+        {
+            this.Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            this.Saldo -= quantia;
+        }
+    }
+}
