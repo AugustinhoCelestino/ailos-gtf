c385ad69b70e313c27b35b66b74151038635c9d5
AugustinhoCelestino <58995280+AugustinhoCelestino@users.noreply.github.com>
2025-03-29 12:34:37 -0300

    solution for question 2

---
+++ Questao2/FootballMatchesResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Questao2
+{
+    public class FootballMatchesResponse
+    {
+        public int Page { get; set; }
+        public int Per_page { get; set; }
+        public int Total { get; set; }
+        public int Total_pages { get; set; }
+        public List<Matches> Data { get; set; } = new List<Matches>();
+    }
+
+    public class Matches
+    {
+        public string Competition { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Round { get; set; } = string.Empty;
+        public string Team1 { get; set; } = string.Empty;
+        public string Team2 { get; set; } = string.Empty;
+        public string Team1goals { get; set; } = string.Empty;
+        public string Team2goals { get; set; } = string.Empty;
+    }
+
+}
--- Questao2/Program.cs
+++ Questao2/Program.cs
@@ -1,30 +1,83 @@
 ﻿using Newtonsoft.Json;
+using System.Net;
+using System.Web;
 
-public class Program
+namespace Questao2
 {
-    public static void Main()
+    public class Program
     {
-        string teamName = "Paris Saint-Germain";
-        int year = 2013;
-        int totalGoals = getTotalScoredGoals(teamName, year);
+        public static void Main()
+        {
+            string teamName = "Paris Saint-Germain";
+            int year = 2013;
+            int totalGoals = GetTotalScoredGoals(teamName, year);
 
-        Console.WriteLine("Team "+ teamName +" scored "+ totalGoals.ToString() + " goals in "+ year);
+            Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
 
-        teamName = "Chelsea";
-        year = 2014;
-        totalGoals = getTotalScoredGoals(teamName, year);
+            teamName = "Chelsea";
+            year = 2014;
+            totalGoals = GetTotalScoredGoals(teamName, year);
 
-        Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
+            Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
 
-        // Output expected:
-        // Team Paris Saint - Germain scored 109 goals in 2013
-        // Team Chelsea scored 92 goals in 2014
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+            // Output expected:
+            // Team Paris Saint - Germain scored 109 goals in 2013
+            // Team Chelsea scored 92 goals in 2014
+        }
+
+        public static int GetTotalScoredGoals(string team, int year)
+        {
+            int totalScoredGoals = 0;
+            totalScoredGoals += GetTotalScoredGoalsTeams(team, year, "1");
+            totalScoredGoals += GetTotalScoredGoalsTeams(team, year, "2");
+            return totalScoredGoals;
+        }
+
+        public static int GetTotalScoredGoalsTeams(string team, int year, string side)
+        {
+            int totalScoredGoals = 0;
+            int page = 1;
+            int totalpages = 1;
+
+            UriBuilder urlBuilder = new("https://jsonmock.hackerrank.com/api/football_matches");
+
+            do
+            {
+                var urlQuery = HttpUtility.ParseQueryString(urlBuilder.Query);
+                urlQuery["year"] = year.ToString();
+                urlQuery[$"team{side}"] = team.ToString();
+                urlQuery["page"] = page.ToString();
+
+                urlBuilder.Query = urlQuery.ToString();
+
+                HttpRequestMessage httpRequest = new(HttpMethod.Get, urlBuilder.ToString());
+                HttpClient httpClient = new();
+                HttpResponseMessage response = httpClient.SendAsync(httpRequest).Result;
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    FootballMatchesResponse? footballMatchesResponse = JsonConvert.DeserializeObject<FootballMatchesResponse>(response.Content.ReadAsStringAsync().Result);
+                    if (footballMatchesResponse == null)
+                        break;
+
+                    totalpages = footballMatchesResponse.Total_pages;
+
+                    if (side == "1")
+                        totalScoredGoals += footballMatchesResponse.Data.Sum(s => int.Parse(s.Team1goals));
+                    else
+                        totalScoredGoals += footballMatchesResponse.Data.Sum(s => int.Parse(s.Team2goals));
+                }
+                page++;
+
+            } while (page <= totalpages);
+
+            return totalScoredGoals;
+        }
+
     }
-
-    public static int getTotalScoredGoals(string team, int year)
-    {
-        
-        return 0;
-    }
-
 }
